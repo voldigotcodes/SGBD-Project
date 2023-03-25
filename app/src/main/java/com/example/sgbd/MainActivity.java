b59bd5c453925c4f1ac9ced0004f70502ec62ef6
KoUrbain <okodj030@uottawa.ca>
2023-03-25 13:27:21 -0400

    run the app and look at the logcat

--- app/src/main/java/com/example/sgbd/MainActivity.java
+++ app/src/main/java/com/example/sgbd/MainActivity.java
@@ -21,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
 
+    public void test(View view){
+
+        Database db = new Database();
+        Intent intent = new Intent(getApplicationContext(), Test.class);
+        startActivity(intent);
+        Connection connection = db.getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String query = "SELECT * FROM testtable1";
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                int salary = resultSet.getInt("salary");
+                System.out.println("ID: " + id + ", Name: " + name + ", Age: " + age + ", Salary: " + salary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
