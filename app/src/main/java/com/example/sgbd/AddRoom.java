6dc14e5cfe2bd62ca6747a5c264b57e16a94e1bb
KoUrbain <okodj030@uottawa.ca>
2023-03-31 17:57:51 -0400

    Adds

--- app/src/main/java/com/example/sgbd/AddRoom.java
+++ app/src/main/java/com/example/sgbd/AddRoom.java
@@ -18,7 +18,7 @@ public class AddRoom extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_room1);
+        setContentView(R.layout.activity_add_room);
     }
 
     public void addRoom(View view){
