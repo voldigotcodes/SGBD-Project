84be3bf98910a8511d6d3219575e60b43d2b0453
KoUrbain <okodj030@uottawa.ca>
2023-04-07 00:04:37 -0400

    clients

---
+++ app/src/main/java/com/example/sgbd/ShowClients.java
@@ -0,0 +1,14 @@
+package com.example.sgbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowClients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_clients);
+    }
+}
